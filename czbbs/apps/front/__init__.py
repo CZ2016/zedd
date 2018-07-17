c0bc3a62ef5f2d260b972ec81b4f867c4fca16c5
ccczzz <13732913015>
2018-07-17 17:52:09 +0800

    使用supervisor管理celery

--- czbbs/apps/front/__init__.py
+++
@@ -1,4 +0,0 @@
-#encoding:utf-8
-
-from .views import bp
-import apps.front.hooks
--- czbbs/utils/safeutils.py
+++
@@ -1,9 +0,0 @@
-#encoding: utf-8
-
-from urllib.parse import urlparse,urljoin
-from flask import request
-
-def is_safe_url(target):
-    ref_url = urlparse(request.host_url) #对url进行解析，分成域名部分，path部分和参数部分
-    test_url = urlparse(urljoin(request.host_url, target)) #对传进来的target进行组装并解析
-    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
